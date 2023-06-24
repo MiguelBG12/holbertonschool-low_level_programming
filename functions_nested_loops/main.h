6719b70ff0794446ffea784dc159f694cb9e1c9d
MiguelBG12 <miguel.bg12@gmail.com>
2023-06-23 22:04:02 -0700

    A function that checks for lowercase character.

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,10 +1,12 @@
 #ifndef main_h
 #define main_h
-/** 
- *  main - check the code*
- *  Return: 0 (Success)
+/**
+ * main - check the code
+ *
+ * Return: 0 (Success)
  */
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
 int _putchar(char c);
 #endif
