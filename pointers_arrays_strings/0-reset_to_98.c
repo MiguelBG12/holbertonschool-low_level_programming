c27610cc62602d6c619ec5025fd7158359f9a31a
MiguelBG12 <miguel.bg12@gmail.com>
2023-06-26 20:22:58 -0700

    A function that takes a pointer to an int as parameter and updates the value

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - check a digit
+ * @n: parameter
+ *
+ * Return: Always 0 success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef _main_h
+#define _main_h
+/**
+ * main - check the code
+ * @n: first parameter
+ * Return: 0 (Success)
+ */
+void reset_to_98(int *n);
+#endif
