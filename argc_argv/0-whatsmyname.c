e645fb3c573d9a70bca894b2f51fa495eeac0808
MiguelBG12 <miguel.bg12@gmail.com>
2023-06-28 09:43:41 -0700

    A program that prints its name, followed by a new line

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * _printf - program that prints its name
+ * If you rename the program, it will print the new name
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("./Mynewnameis\n");
+		return (0);
+}
