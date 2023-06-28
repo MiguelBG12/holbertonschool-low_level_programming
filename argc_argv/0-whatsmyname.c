62944ee52d0ead0c10b6664306484bbea48d0cf6
MiguelBG12 <miguel.bg12@gmail.com>
2023-06-28 11:55:44 -0700

    Fix function

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
 /**
- * main - program to print its name
+ * main - Write a program that prints its name
  * @argc: Number of arguments passed to the program
- * @argv: Arry of strings. Each is one of the arguments that was passed
- * If you rename the program, it will print the new name
+ * @argv: Array of strings. Each is one of the arguments that was passed
+ *
+ * Description: If you rename the program, it will print the new name
+ * without having to compile it again
  * Return: Zero
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	int i = 0;
+		int i = 0;
 
-	while (argv[0][i] != '\0')
-	{
-		_putchar(argv[0][i]);
-		i++;
-	}
-	_putchar('\n');
-	return (0);
+		while (argv[0][i] != '\0')
+		{
+			putchar(argv[0][i]);
+			i++;
+		}
+		putchar('\n');
+		return (0);
 }
