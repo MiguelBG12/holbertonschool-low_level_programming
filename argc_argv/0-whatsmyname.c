862f267afd3598fcd58f5dcfe18303d911d588a9
MiguelBG12 <miguel.bg12@gmail.com>
2023-06-28 09:58:36 -0700

    Updating

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * _printf - program that prints its name
+ * main - program that prints its name
  * If you rename the program, it will print the new name
  * Return: Always 0.
  */
 int main(void)
 {
-	printf("./0-whatsmyname.c\n");
+	printf("./0-whatsmyname.0\n");
 		return (0);
 }
