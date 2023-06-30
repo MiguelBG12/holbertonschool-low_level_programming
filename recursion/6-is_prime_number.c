50fbeb3f89177ea365c8800adac4b3deceabf6ba
MiguelBG12 <miguel.bg12@gmail.com>
2023-06-29 20:03:20 -0700

    A function that returns 1 if the input integer is a prime number, otherwise return 0

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -15,3 +15,20 @@ int is_prime_number(int n)
 		return (0);
 	return (check_prime(2, n));
 }
+
+/**
+ * check_prime - Calculates if x is a factor of n.
+ * @x: Integer used on the test.
+ * @n: Integer to be tested.
+ *
+ * Description: Checks for x.
+ * Return: Returns zero if factor, 1 otherwise.
+ */
+int check_prime(int x, int n)
+{
+	if (n % x == 0 && x < n)
+		return (0);
+	if (n == x)
+		return (1);
+	return (check_prime(x + 1, n));
+}
